7e920075af856e8fd588cd8c96c855e360f959b2
MGraceLed <mgraceled@gmail.com>
2022-09-01 08:16:35 -0700

    submit Ops Challenge 03

--- class-03.sh
+++ class-03.sh
@@ -7,11 +7,17 @@
 
 # Main
 
-# Prompts user for input directory path.S
+# Prompts user for input directory path.
 echo "Enter directory path"
 
 read -userinput1
 
 # Prompts user for input permissions number (e.g. 777 to perform a chmod 777)
+echo "Enter permission number"
+
+read -userinput2
+
 # Navigates to the directory input by the user and changes all files inside it to the input setting.
+
+
 # Prints to the screen the directory contents and the new permissions settings of everything in the directory. 
