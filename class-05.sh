2cb69f7ff60f0c25fedc9de0dd71ef6fd10c4664
MGraceLed <mgraceled@gmail.com>
2022-09-02 23:11:05 -0700

    submit Ops Challenge 05

---
+++ class-05.sh
@@ -0,0 +1,23 @@
+# Script Name:  Ops 301 Class 05 Challenge Solution  
+# Author:   MaryGrace Ledesma
+# Date of last revision: September 02, 2022
+# Description of purpose: Write a script that will clear a content of a log file
+# Declaration of variables:
+# Declaration of functions:
+
+
+# Main
+
+
+# Write a bash script that clears the contents of these logs:
+
+filesyslog=/var/log/syslog
+filewtmp=/var/log/wtmp
+
+printf $1 /var/log/syslog > /dev/null
+pringf $2 /var/log/wtmp > /dev/null
+
+# Print to screen the before and after of the contents of each file.
+
+
+# End
