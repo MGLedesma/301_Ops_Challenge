e33915b29bac3e59f72f1c1abfe2af1a6ae57fbb
MGraceLed <mgraceled@gmail.com>
2022-09-08 09:52:40 -0700

    submit ops challenge 07

--- class-07.py
+++ class-07.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# Script Name:  Ops 301 Class 07 Challenge Solution  
+# Author:   MaryGrace Ledesma
+# Date of last revision: September 07, 2022
+# Description of purpose: 
 # Import libraries
 
 import os
@@ -12,13 +16,19 @@ import os
 
 ### Declare a function here
 
-for (root, dirs, files) in os.walk("testdir"):
-    ### Add a print command here to print ==root==
-    print(root)
-    ### Add a print command here to print ==dirs==
-    print(dirs)
-    ### Add a print command here to print ==files==
-    print(files)
+# for (root, dirs, files) in os.walk("testdir"):
+#     ### Add a print command here to print ==root==
+#     print(root)
+#     ### Add a print command here to print ==dirs==
+#     print(dirs)
+#     ### Add a print command here to print ==files==
+#     print(files)
+
+def walkfunc(argument):
+    for (root, dirs, files) in os.walk(argument):
+        print(root)
+        print(dirs)
+        print(files)
 
 # Main
 
