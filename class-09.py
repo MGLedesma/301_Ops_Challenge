0c749536303a7555d81708c5a7090f702cfa8071
MGraceLed <mgraceled@gmail.com>
2022-09-10 13:26:45 -0700

    submit ops challenge 09

---
+++ class-09.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Script Name:  Ops 301 Class 09 Challenge Solution  
+# Author:   MaryGrace Ledesma
+# Date of last revision: September 09, 2022
+# Description of purpose: Create a Python Script of List
+
+# Main
+
+# Assign to a variable a list of ten string elements.
+Birds = ["parrots", "owls", "finches", "falcon", "herons", "toucans", "cranes", "pelican", "stork", "swans"]
+
+# Print the fourth element of the list.
+print(Birds[3])
+
+# Print the sixth through tenth element of the list.
+print(-1:-4)
+
+# Change the value of the seventh element to “onion”.
+Birds.append"cranes"="onions"
