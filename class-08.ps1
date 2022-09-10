eb707b77e45cd717ac78d0f11ed2d0a63aaa71b3
MGraceLed <mgraceled@gmail.com>
2022-09-10 13:07:27 -0700

    submit ops challenge 08

--- class-08.ps1
+++ class-08.ps1
@@ -6,13 +6,9 @@
 # Franz Ferdinand is the TPS Reporting Lead at GlobeX USA in Springfield, OR office. Franz is part of the TPS Department. Franz’s email is ferdi@GlobeXpower.com.
 # Test your script. Verify in ADAC that the user was created with the correct attributes. If anything is missing, delete the user in ADAC and re-attempt from Powershell ISE.
 
-# Stretch Goals (Optional Objectives)
-# Pursue stretch goals if you are a more advanced user or have remaining lab time.
-
-# Have your script also create a new group in AD.
-# Have your script also create a new OU in AD.
 
 New-ADUser -Name "Franz Ferdinand" -Department "TPS" -Company "GlobeX USA" -Location "Springfield, OR" -UserPrincipalName "ferdi@GlobeXpower.com"
+
 
 
 
