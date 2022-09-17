2408d01faef866163fb0dc0c1c8395f51644652f
MGraceLed <mgraceled@gmail.com>
2022-09-16 23:26:50 -0700

    submit ops challenge 14

---
+++ oc14.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# Script Name:  Ops 301 Class 14 Challenge Solution  
+# Author:   MaryGrace Ledesma
+# Date of last revision: September 14, 2022
+# Description of purpose: Create a Python Scrip
+
