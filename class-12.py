e8faf3a30852765fc18d4f303843890b190fbc92
MGraceLed <mgraceled@gmail.com>
2022-09-21 11:28:19 -0700

    update ops challenge 12

--- class-12.py
+++ class-12.py
@@ -10,13 +10,7 @@ import psutil
 # Create a Python script that fetches this information using Psutil:
 
 # Time spent by normal processes executing in user mode
-psutil.cpu_times()
-print(psutil.cpu_times())
-
 # Time spent by processes executing in kernel mode
-krnelmode = psutil.cpu_times(system)
-print(krnelmode)
-
 # Time when system was idle
 # Time spent by priority processes executing in user mode
 # Time spent waiting for I/O to complete.
@@ -24,3 +18,5 @@ print(krnelmode)
 # Time spent for servicing software interrupts
 # Time spent by other operating systems running in a virtualized environment
 # Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
+
+psutil.cpu_times()
