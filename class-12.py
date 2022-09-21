4f5ad638b17e7baa19bb0964d5c884907c7e4626
MGraceLed <mgraceled@gmail.com>
2022-09-21 09:36:01 -0700

    update ops challenge 12

---
+++ class-12.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+# Script Name:  Ops 301 Class 12 Challenge Solution  
+# Author:   MaryGrace Ledesma
+# Date of last revision: September 14, 2022
+# Description of purpose: Create a Python Script that acess system details and process utilities
+
+import psutil
+
+# Create a Python script that fetches this information using Psutil:
+
+# Time spent by normal processes executing in user mode
+psutil.cpu_times()
+print(psutil.cpu_times())
+
+# Time spent by processes executing in kernel mode
+krnelmode = psutil.cpu_times(system)
+print(krnelmode)
+
+# Time when system was idle
+# Time spent by priority processes executing in user mode
+# Time spent waiting for I/O to complete.
+# Time spent for servicing hardware interrupts
+# Time spent for servicing software interrupts
+# Time spent by other operating systems running in a virtualized environment
+# Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
