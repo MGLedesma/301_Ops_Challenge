889e12beb7faaa602d28429b20df2be5dda243e1
MGraceLed <mgraceled@gmail.com>
2022-09-22 05:38:20 -0700

    update ops challenge 12

--- class-12.py
+++ class-12.py
@@ -7,16 +7,34 @@
 
 import psutil
 
-# Create a Python script that fetches this information using Psutil:
+#Declaration of variables
+cpuinfo = psutil.cpu_times()
 
-# Time spent by normal processes executing in user mode
-# Time spent by processes executing in kernel mode
-# Time when system was idle
-# Time spent by priority processes executing in user mode
-# Time spent waiting for I/O to complete.
-# Time spent for servicing hardware interrupts
-# Time spent for servicing software interrupts
+# Time spent by normal processes executing in user mode = user parameter
+print("Time spent by normal processes executing in user mode:", cpuinfo[0])
+
+# Time spent by processes executing in kernel mode = system parameter
+print("Time spent by processes executing in kernel mode:", cpuinfo[2])
+
+# Time when system was idle = idle parameter
+print("Time when system was idle:", cpuinfo[3])
+
+# Time spent by priority processes executing in user mode = nice parameter
+print("Time spent by priority processes executing in user mode:", cpuinfo[1])
+
+# Time spent waiting for I/O to complete = iowait parameter
+print("Time spent waiting for I/O to complete:", cpuinfo[4])
+
+# Time spent for servicing hardware interrupts = irq parameter
+print("Time spent for servicing hardware interrupts:", cpuinfo[5])
+
+# Time spent for servicing software interrupts = softirq parameter
+print("Time spent for servicing software interrupts:", cpuinfo[6])
+
 # Time spent by other operating systems running in a virtualized environment
-# Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
+print("Time spent by other operating systems running in a virtualized environment:", cpuinfo[7])
 
-print(psutil.cpu_times())
+# Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
+print("Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel:", cpuinfo[8])
+
+# End
